42bb8ff2cf1a7ede0fe308454e5fa43ccd06dcb6
Pavel Dzhazga <dzhazga2002@mail.ru>
2023-02-21 20:32:44 +0300

    fixnow

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,8 +1,8 @@
-package controller;
+package web.controller;
 
-import model.Car;
+import web.model.Car;
 import org.springframework.beans.factory.annotation.Autowired;
-import service.CarService;
+import web.service.CarService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
