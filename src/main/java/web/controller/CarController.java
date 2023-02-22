0b1d4672e2ae4d47b7cb55c254e5889a4f5e8a9e
Pavel Dzhazga <dzhazga2002@mail.ru>
2023-02-22 15:05:18 +0300

    fixnow

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,19 +1,16 @@
 package web.controller;
 
-import web.model.Car;
-import org.springframework.beans.factory.annotation.Autowired;
 import web.service.CarService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.service.CarServiceImpl;
 
 
 @Controller
 public class CarController {
 
-    private CarService carService;
+    private final CarService carService;
 
     public CarController(CarService carService) {
         this.carService = carService;
